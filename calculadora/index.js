72f2a29f7ec4876a4dd41ea963a6c26797d2d0ee
GustavoManganelli <gugu.manganeli@gmail.com>
2024-06-29 19:52:51 +0000

    escrita das funções em js

---
+++ calculadora/index.js
@@ -0,0 +1,18 @@
+function soma(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+function subtracao(numero1, numero2) {
+    return numero1 - numero2;
+}
+
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+
+function divisao(numero1,numero2) {
+    if (numero2 == 0) {
+        return 0;
+    }
+    return numero1 / numero2;
+}
